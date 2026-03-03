4f9656ad2abcf41dc9cc5486b69a08fd974ffbf9
aluno <aluno@lab01-10>
2026-03-03 14:18:41 -0300

    criação da classe Estudante

---
+++ Programacao01/src/main/java/com/mycompany/programacao01/Estudante.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.programacao01;
+
+import java.util.Date;
+
+/**
+ *
+ * @author aluno
+ */
+public class Estudante {
+    private String nome;
+    private Date Datanascimento;
+    private String cpf;
+    private char genero;
+    private String matricula;
+    private String email;
+
+    
+   public Estudante(){ 
+       nome = "nobody";
+       
+    }
+public int obterIdade(Date hoje){
+    
+    int idade = 0;
+    
+    return idade;
+}
+public String getNome(){
+    return nome;
+}
+public void setNome(String _nome){
+    nome = _nome;
+}
+
+public Date getDatanascimento (){
+    return Datanascimento;
+}
+public void setDatanascimento(Date _Datanascimento){
+    Datanascimento = _Datanascimento;
+}
+public String getCpf(){
+    return cpf;
+}
+public void setCpf(String _cpf){
+    cpf = _cpf;
+}
+
+public char getGenero(){
+    return genero;
+}
+public void setGenero(char _genero){
+    genero = _genero;
+}
+public String getMatricula(){
+    return matricula;
+}
+public void setMatricula(String _matricula){
+    matricula = _matricula;
+}
+public String getEmail(){
+    return email;
+}
+public void setEmail(String _email){
+    email = _email;
+}
+
+}
